b005c724a237399a2732eb0e2d64d9dc19bfe830
Mikayla Clemons <mikaylaclemons@Mikaylas-MacBook-Pro.local>
2019-02-04 09:24:01 -0500

    Create a constructor in Circle class that sets all of its properties using super() to set its inherited properties.

--- src/com/company/Circle.java
+++ src/com/company/Circle.java
@@ -3,4 +3,10 @@ package com.company;
 public class Circle extends Shape {
 
     private double radius;
-}
+
+    public Circle(double radius, String color, boolean filled){
+        super(color, filled);
+        this.radius = radius;
+    }
+    }
+
